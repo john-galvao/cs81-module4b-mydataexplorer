ea790b8b7314a80e47d694cc5baa2866d2dc4822
john-galvao <galvao_22@yahoo.com>
2026-01-15 23:49:04 -0800

    add custom array data

---
+++ myDataJournal.js
@@ -0,0 +1,9 @@
+const weekData = [
+  { day: "Monday", sleepHours: 6, screenTime: 2, mood: "Hungry", caffeineIntake: 0, focusLevel: 5 },
+  { day: "Tuesday", sleepHours: 5, screenTime: 1, mood: "Tired", caffeineIntake: 0, focusLevel: 5 },
+  { day: "Wednesday", sleepHours: 6, screenTime: 2, mood: "Motivated", caffeineIntake: 0, focusLevel: 7 },
+  { day: "Thursday", sleepHours: 6, screenTime: 1, mood: "Focused", caffeineIntake: 1, focusLevel: 8 },
+  { day: "Friday", sleepHours: 6, screenTime: 1, mood: "Calm", caffeineIntake: 1, focusLevel: 9 },
+  { day: "Saturday", sleepHours: 7, screenTime: 1, mood: "Relaxed", caffeineIntake: 0, focusLevel: 5 },
+  { day: "Sunday", sleepHours: 8, screenTime: 2, mood: "Rested", caffeineIntake: 0, focusLevel: 5 }
+];
